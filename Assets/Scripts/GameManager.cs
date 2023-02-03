70732dc3ab045271bf412326a956b52fcd6d980d
Isael <isaelgsfaria@gmail.com>
2023-02-03 16:50:28 -0300

    mouse focus mode added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    
 
     void Awake()
     {
@@ -16,12 +17,21 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.SetMouseFocusMode(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Cancel")) GameManager.SetMouseFocusMode(false);
+        if(Input.GetButtonDown("Submit")) GameManager.SetMouseFocusMode(true);
+    }
+
+    public static void SetMouseFocusMode(bool focus)
+    {
+        if(Cursor.visible != focus) return;
+        Cursor.lockState = (focus)? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !focus;
+        Debug.Log((focus)? "Focus" : "Unfocus");
     }
 }
